bb7617e145a23c39bd0f33756050afb5d35a7d72
Mothusim <mothusim97@gmail.com>
2023-10-10 09:57:26 +0200

    test 2

--- pages/recipes/[recipeId].js
+++ pages/recipes/[recipeId].js
@@ -1,25 +1,25 @@
 // api/recipes.js (API route for fetching recipes with pagination)
 
-import { getAllRecipes, DBConnection } from "../../../helpers/mongoDB-utils";
+// import { getAllRecipes, DBConnection } from "../../../helpers/mongoDB-utils";
 
-const ITEMS_PER_PAGE = 20; //Recipies to be rendered on each page
+// const ITEMS_PER_PAGE = 20; //Recipies to be rendered on each page
 
-const handler = async (req, res) => {
+// const handler = async (req, res) => {
     
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-  try {
-    let client = await DBConnection();
-    const page = req.query.page ? parseInt(req.query.page) : 1;
-    const skip = (page - 1) * ITEMS_PER_PAGE;
-    const recipesData = await getAllRecipes(client, skip, ITEMS_PER_PAGE);
-    res.status(200).json({ recipes: recipesData });
-    client.close();
-  } catch (error) {
-    console.error("Error fetching recipes:", error);
-    res.status(408).json({ error: "Error fetching recipes" });
-  }
-};
+//   if (req.method !== "GET") {
+//     return res.status(405).json({ error: "Method not allowed" });
+//   }
+//   try {
+//     let client = await DBConnection();
+//     const page = req.query.page ? parseInt(req.query.page) : 1;
+//     const skip = (page - 1) * ITEMS_PER_PAGE;
+//     const recipesData = await getAllRecipes(client, skip, ITEMS_PER_PAGE);
+//     res.status(200).json({ recipes: recipesData });
+//     client.close();
+//   } catch (error) {
+//     console.error("Error fetching recipes:", error);
+//     res.status(408).json({ error: "Error fetching recipes" });
+//   }
+// };
 
-export default handler;
+// export default handler;
